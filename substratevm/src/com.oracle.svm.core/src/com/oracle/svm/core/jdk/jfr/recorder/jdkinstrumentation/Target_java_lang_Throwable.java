d01c7ecec60dcf980098a2e59bf0e76ccc558ee5
Simon Tooke <stooke@redhat.com>
2021-01-26 10:30:57 -0500

    fix Error event

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/jfr/recorder/jdkinstrumentation/Target_java_lang_Throwable.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/jfr/recorder/jdkinstrumentation/Target_java_lang_Throwable.java
@@ -1,21 +1,28 @@
 package com.oracle.svm.core.jdk.jfr.recorder.jdkinstrumentation;
 
+import com.oracle.svm.core.SubstrateUtil;
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.NeverInline;
 import com.oracle.svm.core.annotate.RecomputeFieldValue;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
-import com.oracle.svm.core.annotate.TargetElement;
-import com.oracle.svm.core.jdk.JDK8OrEarlier;
 import com.oracle.svm.core.jdk.StackTraceUtils;
 import com.oracle.svm.core.jdk.jfr.JfrAvailability;
 import com.oracle.svm.core.snippets.KnownIntrinsics;
-import jdk.jfr.internal.instrument.ThrowableTracer;
 
 import java.util.List;
 
 import static com.oracle.svm.core.annotate.RecomputeFieldValue.Kind.Reset;
 
+/*
+ * JFR:TODO - Graal substitutions require final classes that inherit only from Object,
+ *    therefore no way to instrument java.lang.Error.
+ *    Here, we have to do a roundabout way of 'isinstanceof()'.
+ */
+/*
+ * JFR:TODO - Make this optional based on Flight Recorder configuration.
+ */
+@SuppressWarnings("ConstantConditions")
 @TargetClass(value = java.lang.Throwable.class, onlyWith = JfrAvailability.WithJfr.class)
 public final class Target_java_lang_Throwable {
 
@@ -47,28 +54,13 @@ public final class Target_java_lang_Throwable {
     }
 
     @Substitute
-    @TargetElement(onlyWith = JDK8OrEarlier.class)
-    int getStackTraceDepth() {
-        if (stackTrace != null) {
-            return stackTrace.length;
-        }
-        return 0;
-    }
-
-    @Substitute
-    @TargetElement(onlyWith = JDK8OrEarlier.class)
-    StackTraceElement getStackTraceElement(int index) {
-        if (stackTrace == null) {
-            throw new IndexOutOfBoundsException();
-        }
-        return stackTrace[index];
-    }
-
-
-    @Substitute
     public Target_java_lang_Throwable() {
         fillInStackTrace();
         Target_jdk_jfr_internal_instrument_ThrowableTracer.traceThrowable(this, null);
+        if (Error.class.isAssignableFrom(this.getClass())) {
+            Error thiz = SubstrateUtil.cast(this, Error.class);
+            Target_jdk_jfr_internal_instrument_ThrowableTracer.traceError(thiz, detailMessage);
+        }
     }
 
     @Substitute
@@ -76,6 +68,10 @@ public final class Target_java_lang_Throwable {
         fillInStackTrace();
         detailMessage = message;
         Target_jdk_jfr_internal_instrument_ThrowableTracer.traceThrowable(this, message);
+        if (Error.class.isAssignableFrom(this.getClass())) {
+            Error thiz = SubstrateUtil.cast(this, Error.class);
+            Target_jdk_jfr_internal_instrument_ThrowableTracer.traceError(thiz, detailMessage);
+        }
     }
 
     @Substitute
@@ -84,6 +80,10 @@ public final class Target_java_lang_Throwable {
         detailMessage = message;
         this.cause = cause;
         Target_jdk_jfr_internal_instrument_ThrowableTracer.traceThrowable(this, message);
+        if (Error.class.isAssignableFrom(this.getClass())) {
+            Error thiz = SubstrateUtil.cast(this, Error.class);
+            Target_jdk_jfr_internal_instrument_ThrowableTracer.traceError(thiz, detailMessage);
+        }
     }
 
     @Substitute
@@ -92,6 +92,10 @@ public final class Target_java_lang_Throwable {
         detailMessage = (cause==null ? null : cause.toString());
         this.cause = cause;
         Target_jdk_jfr_internal_instrument_ThrowableTracer.traceThrowable(this, detailMessage);
+        if (Error.class.isAssignableFrom(this.getClass())) {
+            Error thiz = SubstrateUtil.cast(this, Error.class);
+            Target_jdk_jfr_internal_instrument_ThrowableTracer.traceError(thiz, detailMessage);
+        }
     }
 
     @Substitute
@@ -109,6 +113,10 @@ public final class Target_java_lang_Throwable {
             suppressedExceptions = null;
         }
         Target_jdk_jfr_internal_instrument_ThrowableTracer.traceThrowable(this, message);
+        if (Error.class.isAssignableFrom(this.getClass())) {
+            Error thiz = SubstrateUtil.cast(this, Error.class);
+            Target_jdk_jfr_internal_instrument_ThrowableTracer.traceError(thiz, detailMessage);
+        }
     }
 
     @TargetClass(className = "jdk.jfr.internal.instrument.ThrowableTracer", onlyWith = JfrAvailability.WithJfr.class)
